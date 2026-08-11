98d9c225306b0af6142c9401fc4737ae6ef7f96e
Kunal Upadhyay <kunalupadhyay2602@gmail.com>
2026-08-12 04:50:29 +0530

    Set target FAR to 1% for threshold calibration

--- thres.py
+++ thres.py
@@ -5,7 +5,7 @@ scores = df.score
 
 fpr, tpr, thresholds = roc_curve(y, scores)
 
-# choose threshold at FAR ≈ 1%
+
 target_far = 0.01
 idx = (fpr <= target_far).nonzero()[0][-1]
 tau_live = thresholds[idx]
