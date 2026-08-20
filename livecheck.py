721422400ebd311f4b3aa5dde8867d52fbb96d47
Kunal Upadhyay <kunalupadhyay2602@gmail.com>
2026-08-21 04:45:17 +0530

    Update livecheck.py

--- livecheck.py
+++ livecheck.py
@@ -500,7 +500,7 @@ def analyze_video(
         blink_ok = True
         mouth_ok = True
 
-    tau_live = 0.80
+    tau_live = 0.67708
     base_live = (score >= tau_live)
     decision = "LIVE" if (base_live ) else "SPOOF"
 
